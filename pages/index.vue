16a9137dd60d63e744fec79b1a8c725da1abd0ed
hixb <Hello16369@163.com>
2024-01-06 20:22:33 +0800

    chore(lint): using eslint

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const a = 1
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Home page content goes here</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
