7c7e1f70cc33f9b96ebaa168849a60872fd4a878
hixb <Hello16369@163.com>
2024-01-06 20:46:47 +0800

    style: add google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400&display=swap' },
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://fonts.font.im/css?family=Barlow|Roboto+Mono|Titillium+Web',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   css: [
     '~/assets/css/tailwind.scss',
